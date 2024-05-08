4a5178dc5697714460b70d6fabfc1e0a46fd9b39
ivonncita <ivonne.aeg.nepal@gmail.com>
2024-05-07 20:46:47 -0600

    cambio

--- kiwi.js
+++ kiwi.js
@@ -1,5 +1,5 @@
 document.getElementById("myButton").style.backgroundColor = "red";
 
-var esposo = "Rolando";
+var esposo = "Rolando1";
 var esposa = "Ivonncita"; 
 alert ("yo soy " + esposa + " y mi esposo es " + esposo);
