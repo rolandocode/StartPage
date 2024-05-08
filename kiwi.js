469ad533dec0346640b0cb0903a8bb36c1247e36
ivonncita <ivonne.aeg.nepal@gmail.com>
2024-05-07 20:39:41 -0600

    Cambio en nombre de esposos

--- kiwi.js
+++ kiwi.js
@@ -1,5 +1,5 @@
 document.getElementById("myButton").style.backgroundColor = "red";
 
-var esposo = "rolando";
-var esposa = "ivonncita"; 
+var esposo = "Rolando";
+var esposa = "Ivonncita"; 
 alert ("yo soy " + esposa + " y mi esposo es " + esposo);
