0b767ff1340d83a9d21a1c99070e6492f83ea312
dev-rvdl <dev-rvdl@outlook.es>
2024-05-07 20:47:16 -0600

    Otro cambio

--- kiwi.js
+++ kiwi.js
@@ -1,5 +1,5 @@
 document.getElementById("myButton").style.backgroundColor = "red";
 
 var esposo = "Rolando1";
-var esposa = "Ivonncita"; 
+var esposa = "Ivonncita1"; 
 alert ("yo soy " + esposa + " y mi esposo es " + esposo);
